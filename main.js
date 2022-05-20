caa514b6b6b35f496417e650da2d4163009c4314
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-05-20 22:14:37 +0300

    edited

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ let header = document.querySelector('.header');
 let hamburgerMenu = document.querySelector('.hamburger-menu');
 
 hamburgerMenu.addEventListener('click', function () {
-    header.classList.toggle('menu-open')
+    header.classList.toggle('menu-open');
 })
